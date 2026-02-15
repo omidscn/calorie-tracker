5d2867923452b6bd2ac19e9e3bb1ea12cb4ab434
Omid <omid.scn@gmail.com>
2026-02-15 09:12:54 +0100

    Initial Commit

---
+++ calorie-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calorie-tracker
+//
+//  Created by Omid on 15.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calorie-tracker/calorie_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  calorie_trackerApp.swift
+//  calorie-tracker
+//
+//  Created by Omid on 15.02.26.
+//
+
+import SwiftUI
+
+@main
+struct calorie_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
